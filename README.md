4d0d015063c9a54e1e0fcbea6e0f2b1cb76951c0
TristanLobaugh <ttrader_atl@yahoo.com>
2016-05-15 22:20:22 -0400

    init commits

---
+++ README.md
@@ -0,0 +1,35 @@
+# Bookmarks
+
+### Bookmarks app build using AngularJS for the front-end, Express for back-end and Mongoose as the database.
+
+## Summary
+
+#### MORE DETAILED EXPLANATION.
+
+### Author: Tristan Lobaugh 
++ Github - https://github.com/TristanLobaugh
++ Homepage - http://tristanlobaugh.com
+
+## Demo
+
+[Live Demo](http://tristanlobaugh.com/)
+
+## Screenshots
+
+### Main page:
+![alt text](https://raw.githubusercontent.com/TristanLobaugh/******/master/img/screen_shot.png)
+
+
+##Code Examples
+
+### CODE EXPLANATION
+```
+
+```
+
+### CODE EXPLANATION
+```
+
+```
+
+## To Do
---
+++ front_end/js/controller.js
@@ -0,0 +1,57 @@
+var bookmarksApp = angular.module("bookmarksApp", ["ngRoute"]);
+
+bookmarksApp.config(function($routeProvider){
+	$routeProvider.when("/", {
+		templateUrl: "pages/main.html",
+		controller: "mainController"
+	});
+	// $routeProvider.when("/edit", {
+	// 	templateUrl: "pages/edit.html",
+	// 	controller: "mainController"
+	// });
+	$routeProvider.otherwise({
+		redirectTo: "/"
+	});
+});
+
+bookmarksApp.controller("mainController", function($scope, $http, $location){
+	getBookmarks();
+	var myUrl = "http://localhost:3090/"
+
+//call to backend to get the list of bookmarks
+	function getBookmarks(){
+		console.log("getting bookmarks")
+		var apiUrl = myUrl + "get_bookmarks";
+		$http.get(apiUrl)
+		.then(function successCallback(response){
+			console.log(response);
+			if(response.data ==  null){
+
+			}else{
+				$scope.bookmarks = response.data.bookmarks;
+			}
+		}, function errorCallback(response){
+			$scope.result = "ERROR!!! "	+ response.status;
+			// console.log($scope.result);
+		});
+	}
+
+//post to backend to add a bookmark
+	$scope.addBookmark = function(){
+
+	}
+
+//post to backend to remove a bookmark
+	$scope.removeBookmark = function(){
+
+	}
+
+//post to backend when bookmark is naviagted to to increment view count
+	$scope.gotoBookmark = function(){
+
+	}
+
+
+
+// END CONTROLLER
+});
